ba571cebbd093f43ba17bcb33bf0fead143d5b1c
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-03 16:44:08 +0600

    UserList updated, delete user feature added

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -4,14 +4,16 @@ import UserItem from './UserItem/UserItem';
 
 interface UserListProps {
   users: User[];
+  onItemDeleteClick: (id: number) => void;
 }
 
-const UserList: FC<UserListProps> = ({ users }) => {
+const UserList: FC<UserListProps> = ({ users, onItemDeleteClick }) => {
   return (
     <div>
+      <h3>User List</h3>
       <div className='list-group'>
         {users.map((x) => (
-          <UserItem key={x.id} user={x} />
+          <UserItem key={x.id} user={x} onDeleteClick={() => onItemDeleteClick(x.id)} />
         ))}
       </div>
     </div>
