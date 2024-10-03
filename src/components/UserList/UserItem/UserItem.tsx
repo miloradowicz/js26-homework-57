2badd8e67f2698bd3e2fa5723611ba9d95c407ef
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-03 16:43:51 +0600

    UserItem updated, delete user feature added, font-awesome dependency added

--- src/components/UserList/UserItem/UserItem.tsx
+++ src/components/UserList/UserItem/UserItem.tsx
@@ -1,18 +1,37 @@
-import { FC } from 'react';
+import { FC, MouseEventHandler } from 'react';
 import { User } from '../../../types';
 
 interface UserItemProps {
   user: User;
+  onDeleteClick: MouseEventHandler;
 }
 
-const UserItem: FC<UserItemProps> = ({ user }) => {
+const UserItem: FC<UserItemProps> = ({ user, onDeleteClick }) => {
   return (
     <div className='list-group-item'>
-      <span>${user.id}</span>
-      <span>${user.name}</span>
-      <span>${user.email}</span>
-      <span>${user.role}</span>
-      <span>${user.isActive}</span>
+      <div className='row align-items-center'>
+        <h6 className='col-8 offset-2 my-0 p-0'>{user.name}</h6>
+        <button
+          type='button'
+          className='col-2 btn btn-link text-danger fa fa-trash'
+          onClick={onDeleteClick}
+          aria-label='Delete'
+          style={{ textDecoration: 'none' }}
+        />
+      </div>
+      <div>
+        <span className='me-2'>User ID:</span>
+        {user.id}
+      </div>
+      <div>
+        <span className='me-2'>Email:</span>
+        {user.email}
+      </div>
+      <div>
+        <span className='me-2'>Role:</span>
+        {user.role}
+      </div>
+      <div className={user.isActive ? 'text-success' : 'text-danger'}>{user.isActive ? 'Active' : 'Not active'}</div>
     </div>
   );
 };
